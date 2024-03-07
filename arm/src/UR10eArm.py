ea4298950e7ce24850ca29c9c99fe4fb28a0f30a
Pedro Amaral <pedromiglou@hotmail.com>
2024-03-07 17:19:29 +0000

    UR10eArm movement with services

--- arm/src/UR10eArm.py
+++ arm/src/UR10eArm.py
@@ -1,24 +1,16 @@
-# Python 2/3 compatibility imports
-from __future__ import print_function
-from six.moves import input
+#! /usr/bin/python3
 
-import sys
-import rospy
+import geometry_msgs.msg
 import moveit_commander
 import moveit_msgs.msg
-import geometry_msgs.msg
+import rospy
+import sys
+
+from arm.srv import MoveArmToJointsState, MoveArmToJointsStateResponse
+from arm.srv import MoveArmToPoseGoal, MoveArmToPoseGoalResponse
+from arm.srv import StopArm, StopArmResponse
+from math import dist, fabs, cos
 from moveit_commander.conversions import pose_to_list
-
-
-try:
-    from math import pi, tau, dist, fabs, cos
-except:  # For Python 2 compatibility
-    from math import pi, fabs, cos, sqrt
-
-    tau = 2.0 * pi
-
-    def dist(p, q):
-        return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
 
 
 def all_close(goal, actual, tolerance):
@@ -52,7 +44,7 @@ def all_close(goal, actual, tolerance):
 
 
 class UR10eArm(object):
-    """UR10eArm"""
+    """UR10eArm movement with ROS services"""
 
     def __init__(self):
         super(UR10eArm, self).__init__()
@@ -121,8 +113,23 @@ class UR10eArm(object):
         self.eef_link = eef_link
         self.group_names = group_names
 
+        # flag indicating the robot started stopping
+        self.stopping = False
 
-    def go_to_joint_state(self, joint1, joint2, joint3, joint4, joint5, joint6, vel, a):
+        # initialize services
+        move_arm_to_pose_goal_service = rospy.Service("move_arm_to_pose_goal", MoveArmToPoseGoal, self.move_arm_to_pose_goal)
+        move_arm_to_joints_state_service = rospy.Service("move_arm_to_joints_state", MoveArmToJointsState, self.move_arm_to_joints_state)
+        stop_arm_service = rospy.Service("stop_arm", StopArm, self.stop_arm)
+
+    # def go_to_joint_state(self, joint1, joint2, joint3, joint4, joint5, joint6, vel, a):
+    def go_to_joint_state(self, req):
+        """
+        Go to a certain pose goal described by the joints state
+        """
+        assert len(req.goal) == 6 and 0 < req.velocity <= 1 and 0 < req.acceleration <= 1
+
+        self.stopping = False
+
         # Copy class variables to local variables to make the web tutorials more clear.
         # In practice, you should use the class variables directly unless you have a good
         # reason not to.
@@ -137,20 +144,22 @@ class UR10eArm(object):
         ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
         # We get the joint values from the group and change some of the values:
         joint_goal = move_group.get_current_joint_values()
-        joint_goal[0] = joint1
-        joint_goal[1] = joint2
-        joint_goal[2] = joint3
-        joint_goal[3] = joint4
-        joint_goal[4] = joint5
-        joint_goal[5] = joint6  # 1/6 of a turn
-        # joint_goal[6] = 0
+        joint_goal[0] = req.goal[0]
+        joint_goal[1] = req.goal[1]
+        joint_goal[2] = req.goal[2]
+        joint_goal[3] = req.goal[3]
+        joint_goal[4] = req.goal[4]
+        joint_goal[5] = req.goal[5]
 
-        move_group.set_max_velocity_scaling_factor(vel)
-        move_group.set_max_acceleration_scaling_factor(a)
+        # speed configuration
+        move_group.set_max_velocity_scaling_factor(req.velocity)
+        move_group.set_max_acceleration_scaling_factor(req.acceleration)
 
         # The go command can be called with joint values, poses, or without any
         # parameters if you have already set the pose or joint target for the group
-        move_group.go(joint_goal, wait=True)
+        success = False
+        while not success and not self.stopping:
+            success = move_group.go(joint_goal, wait=True)
 
         # Calling ``stop()`` ensures that there is no residual movement
         move_group.stop()
@@ -159,35 +168,49 @@ class UR10eArm(object):
 
         # # For testing:
         current_joints = move_group.get_current_joint_values()
-        return all_close(joint_goal, current_joints, 0.01)
 
+        if all_close(joint_goal, current_joints, 0.01):
+            return MoveArmToJointsStateResponse('Arm is now at requested joints state goal.')
+        else:
+            return MoveArmToJointsStateResponse('Arm is not at requested joints state goal.')
 
-    def go_to_pose_goal(self, trans_x, trans_y, trans_z, q1, q2, q3, q4, vel, a):
+    def go_to_pose_goal(self, req):
         """
         Go to a certain pose goal described by a quaternion tf from 'world' to the end-effector 'tool0'
         """
+        assert len(req.translation) == 3 and len(
+            req.quaternions) == 4 and 0 < req.velocity <= 1 and 0 < req.acceleration <= 1
+        
+        self.stopping = False
+
         move_group = self.move_group
 
         ## Planning to a Pose Goal
         ## ^^^^^^^^^^^^^^^^^^^^^^^
         ## We can plan a motion for this group to a desired pose for the end-effector:
         pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = q4
-        pose_goal.orientation.z = q3
-        pose_goal.orientation.y = q2
-        pose_goal.orientation.x = q1
+        pose_goal.orientation.w = req.quaternions[3]
+        pose_goal.orientation.z = req.quaternions[2]
+        pose_goal.orientation.y = req.quaternions[1]
+        pose_goal.orientation.x = req.quaternions[0]
 
-        pose_goal.position.x = trans_x
-        pose_goal.position.y = trans_y
-        pose_goal.position.z = trans_z
+        pose_goal.position.x = req.translation[0]
+        pose_goal.position.y = req.translation[1]
+        pose_goal.position.z = req.translation[2]
 
-        move_group.set_max_velocity_scaling_factor(vel)
-        move_group.set_max_acceleration_scaling_factor(a)
+        # speed configuration
+        move_group.set_max_velocity_scaling_factor(req.velocity)
+        move_group.set_max_acceleration_scaling_factor(req.acceleration)
 
         ## Now, we call the planner to compute the plan and execute it.
-        plan = move_group.go(pose_goal, wait=True)
+        # `go()` returns a boolean indicating whether the planning and execution was successful.
+        success = False
+        while not success and not self.stopping:
+            success = move_group.go(pose_goal, wait=True)
+
         # Calling `stop()` ensures that there is no residual movement
         move_group.stop()
+
         # It is always good to clear your targets after planning with poses.
         # Note: there is no equivalent function for clear_joint_value_targets()
         move_group.clear_pose_targets()
@@ -196,8 +219,29 @@ class UR10eArm(object):
         # Note that since this section of code will not be included in the tutorials
         # we use the class variable rather than the copied state variable
         current_pose = self.move_group.get_current_pose().pose
-        return all_close(pose_goal, current_pose, 0.01)
 
+        if all_close(pose_goal, current_pose, 0.01):
+            return MoveArmToPoseGoalResponse('Arm is now at requested pose goal.')
+        else:
+            return MoveArmToPoseGoalResponse('Arm is not at requested pose goal.')
+
+    def stop_arm(self, req):
+        # robot started stopping
+        self.stopping = True
+
+        # Copy class variables to local variables to make the web tutorials more clear.
+        # In practice, you should use the class variables directly unless you have a good
+        # reason not to.
+        move_group = self.move_group
+
+        # Calling `stop()` ensures that there is no residual movement
+        move_group.stop()
+
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets()
+        move_group.clear_pose_targets()
+
+        return StopArmResponse('Arm is stopped.')
 
     def display_trajectory(self, plan):
         # Copy class variables to local variables to make the web tutorials more clear.
@@ -224,3 +268,11 @@ class UR10eArm(object):
         display_trajectory_publisher.publish(display_trajectory)
 
         ## END_SUB_TUTORIAL
+
+
+if __name__ == '__main__':
+    rospy.init_node('ur10e_arm', anonymous=True)
+
+    arm = UR10eArm()
+
+    rospy.spin()
