24bc9717cdeb83beab798e6a9abde406c83924d0
Pedro Amaral <pedromiglou@hotmail.com>
2024-06-19 15:55:46 +0100

    cleanup UR10eArm.py code

--- arm/src/UR10eArm.py
+++ arm/src/UR10eArm.py
@@ -1,46 +1,12 @@
 #! /usr/bin/python3
 
-import geometry_msgs.msg
 import moveit_commander
-import moveit_msgs.msg
 import rospy
 import sys
 
 from arm.srv import MoveArmToJointsState, MoveArmToJointsStateResponse
 from arm.srv import MoveArmToPoseGoal, MoveArmToPoseGoalResponse
 from arm.srv import StopArm, StopArmResponse
-from math import dist, fabs, cos
-from moveit_commander.conversions import pose_to_list
-
-
-def all_close(goal, actual, tolerance):
-    """
-    Convenience method for testing if the values in two lists are within a tolerance of each other.
-    For Pose and PoseStamped inputs, the angle between the two quaternions is compared (the angle
-    between the identical orientations q and -q is calculated correctly).
-    @param: goal       A list of floats, a Pose or a PoseStamped
-    @param: actual     A list of floats, a Pose or a PoseStamped
-    @param: tolerance  A float
-    @returns: bool
-    """
-    if type(goal) is list:
-        for index in range(len(goal)):
-            if abs(actual[index] - goal[index]) > tolerance:
-                return False
-
-    elif type(goal) is geometry_msgs.msg.PoseStamped:
-        return all_close(goal.pose, actual.pose, tolerance)
-
-    elif type(goal) is geometry_msgs.msg.Pose:
-        x0, y0, z0, qx0, qy0, qz0, qw0 = pose_to_list(actual)
-        x1, y1, z1, qx1, qy1, qz1, qw1 = pose_to_list(goal)
-        # Euclidean distance
-        d = dist((x1, y1, z1), (x0, y0, z0))
-        # phi = angle between orientations
-        cos_phi_half = fabs(qx0 * qx1 + qy0 * qy1 + qz0 * qz1 + qw0 * qw1)
-        return d <= tolerance and cos_phi_half >= cos(tolerance / 2.0)
-
-    return True
 
 
 class UR10eArm(object):
@@ -49,74 +15,25 @@ class UR10eArm(object):
     def __init__(self):
         super(UR10eArm, self).__init__()
 
-        ## BEGIN_SUB_TUTORIAL setup
-        ##
         ## First initialize `moveit_commander`_ and a `rospy`_ node:
         moveit_commander.roscpp_initialize(sys.argv)
-
 
         # rospy.init_node("move_group_python_interface", anonymous=True)
 
         ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
         ## kinematic model and the robot's current joint states
-        robot = moveit_commander.RobotCommander()
+        self.robot = moveit_commander.RobotCommander()
 
         ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
         ## for getting, setting, and updating the robot's internal understanding of the
         ## surrounding world:
-        scene = moveit_commander.PlanningSceneInterface()
+        self.scene = moveit_commander.PlanningSceneInterface()
 
         ## This interface can be used to plan and execute motions:
         group_name = "manipulator"
-        move_group = moveit_commander.MoveGroupCommander(group_name)
+        self.move_group = moveit_commander.MoveGroupCommander(group_name)
 
-        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-        ## trajectories in Rviz:
-        display_trajectory_publisher = rospy.Publisher(
-            "/move_group/display_planned_path",
-            moveit_msgs.msg.DisplayTrajectory,
-            queue_size=20,
-        )
-
-        ## END_SUB_TUTORIAL
-
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
-        ## Getting Basic Information
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-        # We can get the name of the reference frame for this robot:
-        planning_frame = move_group.get_planning_frame()
-        # print("============ Planning frame: %s" % planning_frame)
-
-        # We can also print the name of the end-effector link for this group:
-        eef_link = move_group.get_end_effector_link()
-        # print("============ End effector link: %s" % eef_link)
-
-        # We can get a list of all the groups in the robot:
-        group_names = robot.get_group_names()
-        # print("============ Available Planning Groups:", robot.get_group_names())
-
-        # Sometimes for debugging it is useful to print the entire state of the
-        # robot:
-        # print("============ Printing robot state")
-        # print(robot.get_current_state())
-        # print("")
-        ## END_SUB_TUTORIAL
-
-        # Misc variables
-        self.box_name = ""
-        self.robot = robot
-        self.scene = scene
-        self.move_group = move_group
-        self.display_trajectory_publisher = display_trajectory_publisher
-        self.planning_frame = planning_frame
-        self.eef_link = eef_link
-        self.group_names = group_names
-
-        # flag indicating the robot started stopping
-        self.stopping = False
-
-        # initialize services
+        # Initialize services
         move_arm_to_pose_goal_service = rospy.Service("move_arm_to_pose_goal", MoveArmToPoseGoal, self.move_arm_to_pose_goal)
         move_arm_to_joints_state_service = rospy.Service("move_arm_to_joints_state", MoveArmToJointsState, self.move_arm_to_joints_state)
         stop_arm_service = rospy.Service("stop_arm", StopArm, self.stop_arm)
@@ -127,171 +44,81 @@ class UR10eArm(object):
         """
         assert len(req.goal) == 6 and 0 < req.velocity <= 1 and 0 < req.acceleration <= 1
 
-        self.stopping = False
-
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        move_group = self.move_group
-
-        ## BEGIN_SUB_TUTORIAL plan_to_joint_state
-        ##
-        ## Planning to a Joint Goal
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^
         ## The Panda's zero configuration is at a `singularity <https://www.quora.com/Robotics-What-is-meant-by-kinematic-singularity>`_, so the first
         ## thing we want to do is move it to a slightly better configuration.
         ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
         # We get the joint values from the group and change some of the values:
-        joint_goal = move_group.get_current_joint_values()
-        joint_goal[0] = req.goal[0]
-        joint_goal[1] = req.goal[1]
-        joint_goal[2] = req.goal[2]
-        joint_goal[3] = req.goal[3]
-        joint_goal[4] = req.goal[4]
-        joint_goal[5] = req.goal[5]
+        goal = self.move_group.get_current_joint_values()
+        goal[0] = req.goal[0]
+        goal[1] = req.goal[1]
+        goal[2] = req.goal[2]
+        goal[3] = req.goal[3]
+        goal[4] = req.goal[4]
+        goal[5] = req.goal[5]
 
         # speed configuration
-        move_group.set_max_velocity_scaling_factor(req.velocity)
-        move_group.set_max_acceleration_scaling_factor(req.acceleration)
+        self.move_group.set_max_velocity_scaling_factor(req.velocity)
+        self.move_group.set_max_acceleration_scaling_factor(req.acceleration)
 
-        # The go command can be called with joint values, poses, or without any
-        # parameters if you have already set the pose or joint target for the group
-        #success = False
-        #while not success and not self.stopping:
+        # Now, we call the planner to compute the plan and execute it.
+        self.move_group.set_joint_value_target(goal)
+        success, plan, planning_time, error_code = self.move_group.plan()
 
-        if not self.stopping:
-            success = move_group.go(joint_goal, wait=True)
+        if success:
+            # filter out the first points of the trajectory to avoid the robot to move to the initial position
+            plan.joint_trajectory.points = [p for p in plan.joint_trajectory.points if p.time_from_start.to_sec() > 1.0]
 
-        # Calling ``stop()`` ensures that there is no residual movement
-        move_group.stop()
+            # execute the trajectory
+            success = self.move_group.execute(plan, wait=False)
 
-        ## END_SUB_TUTORIAL
+            if success:
+                return MoveArmToJointsStateResponse('Arm is moving to the requested pose goal.')
 
-        # # For testing:
-        current_joints = move_group.get_current_joint_values()
-
-        if all_close(joint_goal, current_joints, 0.01):
-            return MoveArmToJointsStateResponse('Arm is now at requested joints state goal.')
+            else:
+                return MoveArmToJointsStateResponse('Failed to execute the planned trajectory.')
+        
         else:
-            return MoveArmToJointsStateResponse('Arm is not at requested joints state goal.')
+            return MoveArmToJointsStateResponse('Failed to plan the trajectory. Error Code: ' + str(error_code))
 
     def move_arm_to_pose_goal(self, req):
         """
         Go to a certain pose goal described by a quaternion tf from 'world' to the end-effector 'tool0'
         """
-        assert len(req.translation) == 3 and len(
-            req.quaternions) == 4 and 0 < req.velocity <= 1 and 0 < req.acceleration <= 1
-        
-        self.stopping = False
-
-        move_group = self.move_group
-
-        ## Planning to a Pose Goal
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## We can plan a motion for this group to a desired pose for the end-effector:
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = req.quaternions[3]
-        pose_goal.orientation.z = req.quaternions[2]
-        pose_goal.orientation.y = req.quaternions[1]
-        pose_goal.orientation.x = req.quaternions[0]
-
-        pose_goal.position.x = req.translation[0]
-        pose_goal.position.y = req.translation[1]
-        pose_goal.position.z = req.translation[2]
+        assert 0 < req.velocity <= 1 and 0 < req.acceleration <= 1
 
         # speed configuration
-        move_group.set_max_velocity_scaling_factor(req.velocity)
-        move_group.set_max_acceleration_scaling_factor(req.acceleration)
+        self.move_group.set_max_velocity_scaling_factor(req.velocity)
+        self.move_group.set_max_acceleration_scaling_factor(req.acceleration)
 
-        ## Now, we call the planner to compute the plan and execute it.
-        # `go()` returns a boolean indicating whether the planning and execution was successful.
-        # success = False
-        # while not success and not self.stopping:
-        #     success = move_group.go(pose_goal, wait=True)
-
-        if not self.stopping:
-            start_state = self.robot.get_current_state()
-            move_group.set_start_state(start_state)
-            move_group.set_pose_target(pose_goal)
-            _, plan, __, ___ = move_group.plan()
-            rospy.logerr(plan)
-            rospy.logerr(start_state)
-            #rospy.logerr(type(plan))
-            #traj = plan.get_robot_trajectory_msg()
-            #plan.set_robot_trajectory(traj)
-            #plan.joint_trajectory.points[0].positions = self.robot.get_current_state().joint_state.position
-            #plan.joint_trajectory.points.pop(0)
-            
-
-            plan.joint_trajectory.points.pop(0)
-            plan.joint_trajectory.points.pop(0)
-            plan.joint_trajectory.points.pop(0)
-            plan.joint_trajectory.points.pop(0)
-            plan.joint_trajectory.points.pop(0)
-            plan.joint_trajectory.points.pop(0)
-            plan.joint_trajectory.points.pop(0)
-            success = move_group.execute(plan, wait=False)
-
-        # Calling `stop()` ensures that there is no residual movement
-        move_group.stop()
+        # Now, we call the planner to compute the plan and execute it.
+        self.move_group.set_pose_target(req.goal)
+        success, plan, planning_time, error_code = self.move_group.plan()
 
         # It is always good to clear your targets after planning with poses.
-        # Note: there is no equivalent function for clear_joint_value_targets()
-        move_group.clear_pose_targets()
+        self.move_group.clear_pose_targets()
 
-        # For testing:
-        # Note that since this section of code will not be included in the tutorials
-        # we use the class variable rather than the copied state variable
-        current_pose = self.move_group.get_current_pose().pose
+        # If the plan was successful, execute it
+        if success:
+            # filter out the first points of the trajectory to avoid the robot to move to the initial position
+            plan.joint_trajectory.points = [p for p in plan.joint_trajectory.points if p.time_from_start.to_sec() > 1.0]
 
-        if all_close(pose_goal, current_pose, 0.01):
-            return MoveArmToPoseGoalResponse('Arm is now at requested pose goal.')
+            # execute the trajectory
+            success = self.move_group.execute(plan, wait=False)
+
+            if success:
+                return MoveArmToPoseGoalResponse('Arm is moving to the requested pose goal.')
+
+            else:
+                return MoveArmToPoseGoalResponse('Failed to execute the planned trajectory.')
+        
         else:
-            return MoveArmToPoseGoalResponse('Arm is not at requested pose goal.')
+            return MoveArmToPoseGoalResponse('Failed to plan the trajectory. Error Code: ' + str(error_code))
 
     def stop_arm(self, req):
-        # robot started stopping
-        self.stopping = True
-
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        move_group = self.move_group
-
-        # Calling `stop()` ensures that there is no residual movement
-        move_group.stop()
-
-        # It is always good to clear your targets after planning with poses.
-        # Note: there is no equivalent function for clear_joint_value_targets()
-        move_group.clear_pose_targets()
+        # Calling `stop()` ensures that the robot stops and there is no residual movement
+        self.move_group.stop()
 
         return StopArmResponse('Arm is stopped.')
-
-    def display_trajectory(self, plan):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        robot = self.robot
-        display_trajectory_publisher = self.display_trajectory_publisher
-
-        ## BEGIN_SUB_TUTORIAL display_trajectory
-        ##
-        ## Displaying a Trajectory
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-        ## group.plan() method does this automatically so this is not that useful
-        ## here (it just displays the same trajectory again):
-        ##
-        ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-        ## We populate the trajectory_start with our current robot state to copy over
-        ## any AttachedCollisionObjects and add our plan to the trajectory.
-        display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-        display_trajectory.trajectory_start = robot.get_current_state()
-        display_trajectory.trajectory.append(plan)
-        # Publish
-        display_trajectory_publisher.publish(display_trajectory)
-
-        ## END_SUB_TUTORIAL
 
 
 if __name__ == '__main__':
