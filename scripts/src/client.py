04455e154c12bb67340c12835969f2653d7477b1
afonsocastro <afonsocastro@ua.pt>
2024-06-28 19:37:28 +0100

    including wood block CAD model in larcc rviz and in robot model. including Socket communication client side (not yet fully working)

---
+++ scripts/src/client.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import socket
+import rospy
+import re
+import tf
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 8080        # The port used by the server
+
+
+def talker():
+    rospy.init_node('wood_block_client')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10) # 10hz
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while not rospy.is_shutdown():
+            s.sendall(b'Hello, world')
+            data = s.recv(1024)
+            print("data: ", data)
+            # print('Received', repr(data))
+            # print(data.decode())
+            x = re.findall(r"[-+]?\d*\.\d+|\d+", data.decode())
+            trans = (float(x[0]), float(x[1]), float(x[2]))
+            quat = (float(x[3]), float(x[4]), float(x[5]), float(x[6]))
+            print('trans: ', trans)
+            print('quat: ', quat)
+            br.sendTransform(trans,
+                             quat,
+                             rospy.Time.now(),
+                             "wood_block",
+                             "camera_right_rgb_optical_frame")
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
